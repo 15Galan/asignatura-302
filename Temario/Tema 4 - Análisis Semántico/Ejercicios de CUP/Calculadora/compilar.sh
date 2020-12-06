d54fa8d8247bcced44c7b3ef0b0fa32782521042
Galán <antoniogalanG15@gmail.com>
2020-12-06 11:35:14 +0100

    Ejercicio de la Calculadora añadido

---
+++ Temario/Tema 4 - Análisis Semántico/Ejercicios de CUP/Calculadora/compilar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+
+echo "===== CUP ====="
+cup Calculadora.cup
+
+echo "==== JFLEX ===="
+jflex Calculadora.lex
+
+echo "==== JAVAs ===="
+javac *.java
+
+echo "==============="
+
