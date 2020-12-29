88aa24b35755bc046618fb1ca6a7201941736f26
Galán <antoniogalanG15@gmail.com>
2020-12-29 21:54:16 +0100

    Archivos iniciales de la Práctica Final añadidos
    Enunciado, archivos de instalación y pruebas de desarrollo.

---
+++ Práctica final/Recursos/Pruebas/e2.pl
@@ -0,0 +1,7 @@
+x=1;
+if ( a != b ) 
+     if ( b == c )
+           x = x+2;
+     else
+           x = x+3;
+print (x);                 
---
+++ Práctica final/Recursos/Pruebas/h2.pl
@@ -0,0 +1,8 @@
+for (i=1; i<10; i=i+1) {
+    k=2;
+    for(j=1; j<k; j=j+i) {
+        k = k*i;
+        x = x+i;
+    }    
+}    
+print(x);
